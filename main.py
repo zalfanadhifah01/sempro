2c7a80715c6859142f7797fe51e5999bca68020d
rixkyduwi <55983435+rixkyduwi@users.noreply.github.com>
2024-05-11 18:12:13 +0700

    update list tratement
    Co-Authored-By: zalfanadhifah01 <105707353+zalfanadhifah01@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,43 +1,78 @@
-
-from process import preparation, generate_response
-from flask import Flask, render_template, request
+from process import preparation,generate_response
+from flask import Flask,render_template,request,redirect,url_for
 
 # download nltk
 preparation()
 
 #Start Chatbot
 app = Flask(__name__)
-
+list_products = [
+    {
+        "id": 1,
+        "nama": "Redmi Note 8",
+        "harga": "$4000"
+    }, {
+        "id": 2,
+        "nama": "Samsung Galaxy",
+        "harga": "$5000"
+    }, {
+        "id": 3,
+        "nama": "iPhone 11",
+        "harga": "$15000"
+    }, {
+        "id": 4,
+        "nama": "Mi Redmi 8",
+        "harga": "$3000"
+    }, {
+        "id": 5,
+        "nama": "Mi Redmi 9",
+        "harga": "$4000"
+    }, {
+        "id": 6,
+        "nama": "Redmi Note 8",
+        "harga": "$4000"
+    }, {
+        "id": 7,
+        "nama": "Samsung Galaxy",
+        "harga": "$5000"
+    }, {
+        "id": 8,
+        "nama": "iPhone 11",
+        "harga": "$15000"
+    }
+]
 @app.route("/")
-def home():
-    return render_template("index.html")
-
+def home(): return render_template("index.html")
 @app.route("/bot")
-def chatbot():
-    return render_template("chatbot.html")
+def chatbot(): return render_template("chatbot.html")
 @app.route("/products")
-def products():
-    return render_template("products.html")
-@app.route("/product_detail")
-def product_detail():
-    return render_template("product_detail.html")
+def products(): return render_template("products.html",list_products=list_products)
+@app.route("/products_detail/<id>")
+def products_detail(id):
+    product_detail = {} 
+    for i in list_products:
+        if i["id"] == id :
+            product_detail.append(i)
+        else:
+            return redirect(url_for('products'))
+    return render_template("product_detail.html",product_detail = product_detail)
 @app.route("/get")
-def get_bot_response():
+def get_bot_response(): 
     user_input = str(request.args.get('msg'))
     print(user_input)
     result = generate_response(user_input)
     return result
 @app.route("/get/berminyak")
-def get_bot_response_berminyak():
+def get_bot_response_berminyak(): 
     jenis_kulit = "berminyak"
     print(jenis_kulit)
     user_input = str(request.args.get('msg'))
     print(user_input)
     result = generate_response(user_input)
-    result= str(result)
+    result = str(result)
     return result
 @app.route("/get/kering")
-def get_bot_response_kering():
+def get_bot_response_kering(): 
     jenis_kulit = "kering"
     print(jenis_kulit)
     user_input = str(request.args.get('msg'))
@@ -46,6 +81,4 @@ def get_bot_response_kering():
     result = str(result)
     return result
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
+if __name__ == "__main__" : app.run(debug = True)

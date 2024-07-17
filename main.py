52f990177bd11b37edca696345c5afd1eb60f31b
rixkyduwi <55983435+rixkyduwi@users.noreply.github.com>
2024-07-17 18:10:35 +0700

    update
    Co-Authored-By: zalfanadhifah01 <105707353+zalfanadhifah01@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify,flash
-import os
-import uuid
-import json
-import random
-import string
-import pickle
+import os,uuid, json,random,string, pickle ,nltk,shutil,torch
 from PIL import Image
-import nltk
 import numpy as np
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 from keras.models import load_model
-import torch
 from torchvision import transforms
 from torchvision.models import resnet50, ResNet50_Weights
 from torch import nn
 from facenet_pytorch import MTCNN
 from copy import deepcopy
-import shutil
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.utils import secure_filename
 
 # Konfigurasi Aplikasi Flask
 app = Flask(__name__)
@@ -55,7 +48,10 @@ def load_user(user_id):
 # Load products from JSON file
 def load_products():
     with open('products.json', 'r') as file:
-        return json.load(file)
+        products = json.load(file)
+        for product in products:
+            product['kategori'] = str(product.get('kategori', ''))  # Ensure 'kategori' is JSON serializable
+        return products
 
 # Save products to JSON file
 def save_bookings(bookings):
@@ -72,30 +68,6 @@ def save_products(products):
     with open('products.json', 'w') as file:
         json.dump(products, file, indent=4)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        users = load_users()
-        user = next((u for u in users if u['username'] == username and u['password'] == password), None)
-        if user:
-            login_user(User(user['id'], user['username'], user['password']))
-            return redirect(url_for('admin'))
-        flash('Invalid credentials')
-    return render_template('admin/login.html')
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
-
-@app.route('/admin')
-@login_required
-def admin():
-    products = load_products()
-    return render_template('admin/index.html', products=products)
 # Variabel Global untuk Chatbot
 global responses, lemmatizer, tokenizer, le, model, input_shape
 input_shape = 11
@@ -145,7 +117,13 @@ def predict(vector):
 def generate_response(text):
     vector = vectorization(text)
     response_tag = predict(vector)
-    if response_tag not in responses:
+    if response_tag == "jenis kulit saya normal":
+        return response_tag
+    elif response_tag == "jenis kulit saya berminyak":
+        return response_tag
+    elif response_tag == "jenis kulit saya kering":
+        return response_tag
+    elif response_tag not in responses:
         return "Sorry, I didn't understand."
     answer = random.choice(responses[response_tag])
     return answer
@@ -153,28 +131,235 @@ def generate_response(text):
 # Persiapan Chatbot
 preparation()
 
-# Route Handlers
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        users = load_users()
+        user = next((u for u in users if u['username'] == username and u['password'] == password), None)
+        if user:
+            login_user(User(user['id'], user['username'], user['password']))
+            return redirect(url_for('edit_product'))
+        flash('Invalid credentials')
+    return render_template('admin/login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+# Static Page Routes
+@app.route('/')
+def home():
+    return render_template('skin_detection.html')
+
+# Route Landing Page
 @app.route("/home")
 def home_view():
     return render_template("index.html")
 
-@app.route("/")
-def home():
-    return render_template("skin_detection.html")
+@app.route('/admin')
+@login_required
+def admin():
+    products = load_products()
+    return render_template('admin/index.html', products=products)
 
+# Chatbot Routes
 @app.route("/bot")
 def chatbot():
     return render_template("chatbot.html")
-
-@app.route("/skin_detection")
-def skin_detection():
-    return redirect(url_for('home'))
 
 @app.route("/get")
 def get_bot_response():
     user_input = str(request.args.get('msg'))
     result = generate_response(user_input)
     return str(result)
+
+# Skin Detection Routes
+@app.route("/skin_detection")
+def skin_detection():
+    return redirect(url_for('home'))
+
+@app.route("/skin_detection_submit", methods=["POST"])
+def skin_detection_submit():
+    file = request.files['gambar']
+    try:
+        img = Image.open(file)
+        if img.mode == 'RGBA':
+            img = img.convert('RGB')
+        random_name = uuid.uuid4().hex + ".jpg"
+        destination = os.path.join(app.config['UPLOAD_FOLDER'], random_name)
+        img.save(destination)
+        hasil = predict_skin(destination)
+        if hasil == False:
+            return jsonify({"msg": "Gagal, Tidak Terdeteksi Wajah"})
+        return jsonify({"msg": "SUKSES", "hasil": hasil, "img": random_name})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+# Products Routes
+@app.route("/products")
+def products():
+    list_products = load_products()
+    all_product = []
+    for product in list_products:
+        if product['id'] in [1, 2, 3, 4, 5]:
+            None
+        else:
+            all_product.append(product)
+    return render_template("products.html", list_products=all_product)
+
+@app.route("/products_detail/<int:id>")
+def products_detail(id):
+    list_products = load_products()
+    list_bookings = load_bookings()
+    print(list_bookings)  # Debugging purposes
+    
+    bookings = [booking for booking in list_bookings if int(booking["product_id"]) == id]
+    print(bookings)  # Debugging purposes
+    
+    product = next((product for product in list_products if product["id"] == id), None)
+    
+    if product:
+        return render_template("product_detail.html", product=product, bookings=bookings)
+    else:
+        return jsonify({"error": "Product not found"}), 404
+
+@app.route("/products_old")
+def products_old():
+    list_products = load_products()
+    list_product_by_treatment = []
+    for product in list_products:
+        if product['id'] in [1, 2, 3, 4, 5]:
+            list_product_by_treatment.append(product)
+    return render_template("products_old.html", list_products=list_product_by_treatment)
+
+@app.route('/api/products', methods=['GET'])
+def get_products():
+    products = load_products()
+    return jsonify(products)
+@app.route('/api/products/<int:product_id>', methods=['GET'])
+def get_product(product_id):
+    products = load_products()
+    product = next((prod for prod in products if prod['id'] == product_id), None)
+    if product is None:
+        return jsonify({'error': 'Product not found'}), 404
+    return jsonify(product)
+
+@app.route('/bookings', methods=['POST'])
+def book():
+    data = request.json
+    bookings = load_bookings()
+    # Check if the booking with the same product_id, date, and time already exists
+    for booking in bookings:
+        if (booking['product_id'] == data['product_id'] and 
+            booking['tanggal'] == data['date'] and 
+            booking['jam'] == data['time']):
+            return jsonify({'message': 'The selected slot is already booked.'}), 409
+    new_booking = {
+        'id': len(bookings) + 1,
+        'product_id': data['product_id'],
+        'product_name': data['productName'],
+        'nama_client': data['name'],
+        'alamat': data['address'],
+        'no_hp': data['phone'],
+        'tanggal': data['date'],
+        'jam': data['time']
+    }
+    bookings.append(new_booking)
+    save_bookings(bookings)
+    return jsonify({'message': 'Booking successful'}), 200
+
+@app.route('/products', methods=['POST'])
+@login_required
+def add_product():
+    products = load_products()  # Muat produk dari file JSON
+    product_name = request.form['productName']
+    product_description = request.form['productDescription']
+    product_price = request.form['productPrice']
+    product_image = request.files['productImage']
+
+    if product_image:
+        filename = secure_filename(product_image.filename)
+        file_url = os.path.join(app.config['UPLOAD_FOLDER'],filename)
+        product_image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    else:
+        file_url = os.path.join(app.config['UPLOAD_FOLDER'],"treatment_5_icon.jpg")
+
+    new_product = {
+        'id': len(products) + 1,
+        'nama': product_name,
+        'deskripsi': product_description,
+        'harga': product_price,
+        'gambar': file_url
+    }
+
+    products.append(new_product)
+    save_products(products)
+    return redirect(url_for('admin'))
+
+@app.route('/bookings')
+@login_required
+def view_bookings():
+    bookings = load_bookings()
+    return render_template('admin/bookings.html', bookings=bookings)
+@app.route("/admin/edit_product")
+@login_required
+def edit_product():
+    list_products = load_products()
+    all_product = []
+    for product in list_products:
+        if product['id'] in [1, 2, 3, 4, 5]:
+            None
+        else:
+            all_product.append(product)
+    print(all_product)
+    return render_template("admin/product_edit.html", list_products=all_product)
+@app.route("/admin/edit_product_detail/<int:id>")
+@login_required
+def edit_product_detail(id):
+    list_products = load_products()
+    list_bookings = load_bookings()
+    print(list_bookings)  # Debugging purposes
+    
+    bookings = [booking for booking in list_bookings if int(booking["product_id"]) == id]
+    print(bookings)  # Debugging purposes
+    
+    product = next((product for product in list_products if product["id"] == id), None)
+    
+    if product:
+        return render_template("admin/product_detail_edit.html", product=product, bookings=bookings)
+    else:
+        return jsonify({"error": "Product not found"}), 404
+
+@app.route('/products/<int:product_id>', methods=['PUT'])
+@login_required
+def update_product(product_id):
+    updated_product = request.json
+    products = load_products()
+    for i, product in enumerate(products):
+        if product['id'] == product_id:
+            products[i] = updated_product
+            save_products(products)
+            return jsonify(updated_product)
+    return jsonify({'error': 'Product not found'}), 404
+
+@app.route('/admin/products/<int:product_id>', methods=['DELETE'])
+@login_required
+def delete_product(product_id):
+    products = load_products()
+    initial_count = len(products)
+    products = [prod for prod in products if prod['id'] != product_id]
+    save_products(products)
+    final_count = len(products)
+    
+    if initial_count == final_count:
+        app.logger.error(f"Produk dengan ID {product_id} tidak ditemukan.")
+    else:
+        app.logger.info(f"Produk dengan ID {product_id} berhasil dihapus.")
+    return jsonify({'message': 'Product deleted'}), 200
 
 @app.route("/rekomendasi_kering")
 def get_rekomendasi_kering():
@@ -209,9 +394,6 @@ def get_rekomendasi_normal():
     rekomendasi += f"1. {list_products[16]['nama']} {list_products[16]['harga']}<br>"
     rekomendasi += f"2. {list_products[20]['nama']} {list_products[20]['harga']}<br><br>"
     return rekomendasi
-@app.route("/skin_detection")
-def skin_detect():
-    return render_template("skin_detection.html")
 
 # Fungsi Prediksi Kulit
 mtcnn = MTCNN(keep_all=False, device='cuda' if torch.cuda.is_available() else 'cpu')
@@ -262,146 +444,6 @@ def predict_skin(image_path):
             return hasil
     else:
         return False
-
-@app.route("/skin_detection_submit", methods=["POST"])
-def skin_detection_submit():
-    file = request.files['gambar']
-    try:
-        img = Image.open(file)
-        if img.mode == 'RGBA':
-            img = img.convert('RGB')
-        random_name = uuid.uuid4().hex + ".jpg"
-        destination = os.path.join(app.config['UPLOAD_FOLDER'], random_name)
-        img.save(destination)
-        hasil = predict_skin(destination)
-        if hasil == False:
-            return jsonify({"msg": "Gagal, Tidak Terdeteksi Wajah"})
-        return jsonify({"msg": "SUKSES", "hasil": hasil, "img": random_name})
-    except Exception as e:
-        return jsonify({"error": str(e)})
-
-@app.route("/products_old")
-def products_old():
-    list_products = load_products()
-    list_product_by_treatment = []
-    for product in list_products:
-        if product['id'] in [1, 2, 3, 4, 5]:
-            list_product_by_treatment.append(product)
-    return render_template("products_old.html", list_products=list_product_by_treatment)
-
-@app.route("/products")
-def products():
-    list_products = load_products()
-    all_product = []
-    for product in list_products:
-        if product['id'] in [1, 2, 3, 4, 5]:
-            None
-        else:
-            all_product.append(product)
-    return render_template("products.html", list_products=all_product)
-@app.route("/products_detail/<int:id>")
-def products_detail(id):
-    list_products = load_products()
-    list_bookings = load_bookings()
-    print(list_bookings)  # Debugging purposes
-    
-    bookings = [booking for booking in list_bookings if int(booking["product_id"]) == id]
-    print(bookings)  # Debugging purposes
-    
-    product = next((product for product in list_products if product["id"] == id), None)
-    
-    if product:
-        return render_template("product_detail.html", product=product, bookings=bookings)
-    else:
-        return jsonify({"error": "Product not found"}), 404
-
-@app.route('/api/products', methods=['GET'])
-def get_products():
-    products = load_products()
-    return jsonify(products)
-
-@app.route('/api/products/<int:product_id>', methods=['GET'])
-def get_product(product_id):
-    products = load_products()
-    product = next((prod for prod in products if prod['id'] == product_id), None)
-    if product is None:
-        return jsonify({'error': 'Product not found'}), 404
-    return jsonify(product)
-
-@app.route('/bookings', methods=['POST'])
-def book():
-    data = request.json
-    bookings = load_bookings()
-    # Check if the booking with the same product_id, date, and time already exists
-    for booking in bookings:
-        if (booking['product_id'] == data['product_id'] and 
-            booking['tanggal'] == data['date'] and 
-            booking['jam'] == data['time']):
-            return jsonify({'message': 'The selected slot is already booked.'}), 409
-    new_booking = {
-        'id': len(bookings) + 1,
-        'product_id': data['product_id'],
-        'product_name': data['productName'],
-        'nama_client': data['name'],
-        'alamat': data['address'],
-        'no_hp': data['phone'],
-        'tanggal': data['date'],
-        'jam': data['time']
-    }
-    bookings.append(new_booking)
-    save_bookings(bookings)
-    return jsonify({'message': 'Booking successful'}), 200
-
-@app.route('/products', methods=['POST'])
-@login_required
-def add_product():
-    new_product = request.json
-    products = load_products()
-    new_product['id'] = max(prod['id'] for prod in products) + 1
-    products.append(new_product)
-    save_products(products)
-    return jsonify(new_product), 201
-@app.route("/edit_product")
-@login_required
-def edit_product():
-    list_products = load_products()
-    return render_template("product_edit.html", list_products=list_products)
-@app.route("/edit_product/<int:id>")
-@login_required
-def edit_product_detail(id):
-    list_products = load_products()
-    list_bookings = load_bookings()
-    print(list_bookings)  # Debugging purposes
-    
-    bookings = [booking for booking in list_bookings if int(booking["product_id"]) == id]
-    print(bookings)  # Debugging purposes
-    
-    product = next((product for product in list_products if product["id"] == id), None)
-    
-    if product:
-        return render_template("product_detail_edit.html", product=product, bookings=bookings)
-    else:
-        return jsonify({"error": "Product not found"}), 404
-
-@app.route('/products/<int:product_id>', methods=['PUT'])
-@login_required
-def update_product(product_id):
-    updated_product = request.json
-    products = load_products()
-    for i, product in enumerate(products):
-        if product['id'] == product_id:
-            products[i] = updated_product
-            save_products(products)
-            return jsonify(updated_product)
-    return jsonify({'error': 'Product not found'}), 404
-
-@app.route('/products/<int:product_id>', methods=['DELETE'])
-@login_required
-def delete_product(product_id):
-    products = load_products()
-    products = [prod for prod in products if prod['id'] != product_id]
-    save_products(products)
-    return jsonify({'message': 'Product deleted'}), 200
 
 def backup_file():
     source = 'products.json'
